61ebbb88d96ba0bece2b39c9694ec9eb7d4d3db3
fionatran <fiona.f.tran@gmail.com>
2022-08-11 08:46:45 -0700

    Updates

---
+++ precache-manifest.6c217a31836678f977d39f8b6092be62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "927698cf0398d74e6eb0",
+    "url": "/wordle-lookup/static/css/main.be88354e.chunk.css"
+  },
+  {
+    "revision": "927698cf0398d74e6eb0",
+    "url": "/wordle-lookup/static/js/main.927698cf.chunk.js"
+  },
+  {
+    "revision": "f63fba0a60bdbf1c75db",
+    "url": "/wordle-lookup/static/js/1.f63fba0a.chunk.js"
+  },
+  {
+    "revision": "9869f3ba1f28e6029b6f",
+    "url": "/wordle-lookup/static/js/runtime~main.9869f3ba.js"
+  },
+  {
+    "revision": "5db4e24a65a7fd1fb4bc8eadf16e2ecd",
+    "url": "/wordle-lookup/static/media/logo.5db4e24a.png"
+  },
+  {
+    "revision": "4551632a40f767c32c3caa36cf56863d",
+    "url": "/wordle-lookup/static/media/wordle_solutions.4551632a.txt"
+  },
+  {
+    "revision": "8daf8621bb1a29548cf7202741eb72fd",
+    "url": "/wordle-lookup/index.html"
+  }
+];
